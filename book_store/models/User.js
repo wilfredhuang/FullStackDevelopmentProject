a6733a2ea4e2aeb04b7402c03a72c966b92a5a64
wilfredhuang <53524711+wilfredhuang@users.noreply.github.com>
2020-05-17 16:45:37 +0800

    Added in Previous Project folders
    - Modified main.handlebar file already, check if anything is missing.
    
    - Added in the css, js files from appdev
    
    - most HTML files are intact and unconverted, except for login and register which i move to 'user'
    
    - Kept the handlebars from practical in 'practical_handlebars'
    
    - Other folders like config, node are copied from practical and unchanged.
    
    Task:
    
    I suggest Bryan and Hasan to work on the main.handlebar and navbar parts since they are more familiar,
    Owen and i can do the routing stuff.

---
+++ book_store/models/User.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const db = require('../config/DBConfig'); // added a ' here 
+
+/* Creates a user(s) table in MySQL Database.     
+Note that Sequelize automatically pleuralizes the entity name as the table name 
+*/
+const User = db.define('user', {
+    name: { type: Sequelize.STRING },
+    email: { type: Sequelize.STRING },
+    password: { type: Sequelize.STRING },
+
+});
+
+
+module.exports = User; 
