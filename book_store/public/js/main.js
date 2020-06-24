341591494f5e610ce68d58203ab436b63e0d2636
Lmao5 <cilipadi270@gmail.com>
2020-06-24 14:42:04 +0800

    Update main.js

--- book_store/public/js/main.js
+++ book_store/public/js/main.js
@@ -1,6 +1,6 @@
 const EasyPost = require("@easypost/api");
 
-const apiKey = "EZTK29b55ab4ee7a437890e19551520f5dd0oJNz9EtGGQwxZFavjxPxCg";
+const apiKey = "EZTK29b55ab4ee7a437890e19551520f5dd0uaJjPiW9XsVqXYFNVI0kog";
 
 const api = new EasyPost(apiKey);
 
