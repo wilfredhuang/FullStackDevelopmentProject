e901881b0859dfec6346b675c706f880227a5664
MoeMaki <64511104+MoeMaki@users.noreply.github.com>
2020-07-13 16:52:34 +0800

    book image

--- book_store/public/js/product_searchbook.js
+++ book_store/public/js/product_searchbook.js
@@ -1,5 +1,6 @@
+const { Json } = require("sequelize/types/lib/utils");
+
 function getBook() {
-    console.log("Test")
     const title = document.getElementById('product_name').value;
     const author = document.getElementById('author');
     const publisher = document.getElementById('publisher');
@@ -7,6 +8,7 @@ function getBook() {
     const price = document.getElementById('price');
     const weight = document.getElementById('weight');
     const details = document.getElementById('details');
+    const product_image = document.getElementById('product_image');
 
     fetch('https://www.googleapis.com/books/v1/volumes?q=' + title + '&maxResults=1&AIzaSyA6Q9oqGie8vr5XgkvlTkhFvufujR2UPOk')
         .then((res) => {
@@ -23,6 +25,7 @@ function getBook() {
             console.log(JSON.stringify(book['items'][0]['volumeInfo']['pageCount'], null, 2));
             console.log(JSON.stringify(book['items'][0]['saleInfo']['saleability'], null, 2));
             */
+            console.log(JSON.stringify(book['items'][0]['volumeInfo']['imageLinks']['thumbnail'], null, 2));
             
             //document.getElementById('product_name').value =JSON.stringify(book['items'][0]['volumeInfo']['title'], null, 2);
             document.getElementById('product_name').value = JSON.stringify(book['items'][0]['volumeInfo']['title'], null, 2).slice(1,JSON.stringify(book['items'][0]['volumeInfo']['title'], null, 2).length-1);
@@ -41,6 +44,7 @@ function getBook() {
             } else {
                 price.value = JSON.stringify(book['items'][0]['saleInfo']['saleability'], null, 2).slice(1,JSON.stringify(book['items'][0]['saleInfo']['saleability'], null, 2).length-1);
             }
+            product_image.value = JSON.stringify(book['items'][0]['volumeInfo']['imageLinks']['thumbnail'], null, 2).slice(1,JSON.stringify(book['items'][0]['volumeInfo']['imageLinks']['thumbnail'], null, 2).length-1);
         });
 }
 
