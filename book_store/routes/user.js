c48f9d7492fb3e883a14feffd7b878571ccbfb44
BryanChinZW <51560465+BryanChinZW@users.noreply.github.com>
2020-07-19 00:11:34 +0800

    verification fix

--- book_store/routes/user.js
+++ book_store/routes/user.js
@@ -29,23 +29,14 @@ let transporter = nodemailer.createTransport({
     },
   });
 
-//jwt middleware
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-  if (token == null) return res.sendStatus(401)
-
-  jwt.verify(token, SECRET, (err, user) => {
-    console.log(err)
-    if (err) return res.sendStatus(403)
-    req.user = user
-    next()
-  })
-}
-
-router.get('/confirmation/:token',authenticateToken,(req,res)=>{
-    user.update({confirmed : true})
-    res.render("/");
+router.get('/confirmation/:token',async(req,res)=>{
+    const token = jwt.verify(req.params.token, SECRET);
+    console.log(token);
+    User.findOne({ where: { id: token.user } })
+        .then(user => {
+            user.update({confirmed:true});
+        })
+    res.redirect("https://localhost:5000/user/login");
 });
 router.get("/auth/facebook", passport.authenticate("facebook",{scope: ['email']}));
 
