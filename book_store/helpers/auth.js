f838387cf98816d579b952dd714200c010be9d28
Lmao5 <cilipadi270@gmail.com>
2020-07-31 19:08:21 +0800

    Minor Update

--- book_store/helpers/auth.js
+++ book_store/helpers/auth.js
@@ -1,10 +1,18 @@
-const alertMessage = require('./messenger'); // Bring in alert messenger
+const alertMessage = require("./messenger"); // Bring in alert messenger
 const ensureAuthenticated = (req, res, next) => {
-    if(req.isAuthenticated()) { // If user is authenticated
-        return next(); // Calling next() to proceed to the next statement
-        }
-        // If not authenticated, show alert message and redirect to ‘/’
-    alertMessage(res, 'danger', 'Access Denied', 'fas fa-exclamation-circle', true);
-    res.redirect('/');
+  if (req.user.confirmed == true) {
+    return next(); // Calling next() to proceed to the next statement
+  } else if (req.user.confirmed === null) {
+    return next();
+  } else {
+    alertMessage(
+      res,
+      "danger",
+      "Access Denied",
+      "fas fa-exclamation-circle",
+      true
+    );
+    res.redirect("/");
+  }
 };
 module.exports = ensureAuthenticated;
