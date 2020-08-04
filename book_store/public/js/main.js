cd0430a16f7f5141d6df2df920f862cfa600058c
Lmao5 <cilipadi270@gmail.com>
2020-08-04 21:00:06 +0800

    Update with direct link to check delivery page from index

--- book_store/public/js/main.js
+++ book_store/public/js/main.js
@@ -3,16 +3,16 @@ const EasyPost = require("@easypost/api");
 const apiKey = "EZTK29b55ab4ee7a437890e19551520f5dd0uaJjPiW9XsVqXYFNVI0kog";
 const api = new EasyPost(apiKey);
 
-const datepicker = document.getElementById('datepicker');
+// const datepicker = document.getElementById('datepicker');
 
-function pickadate() {
-  datepicker.value = moment(new
-    Date(data.Released)).format('DD/MM/YYYY');
-}
+// function pickadate() {
+//   datepicker.value = moment(new
+//     Date(data.Released)).format('DD/MM/YYYY');
+// }
 
-const request = require('request');
-request('http://www.google.com', function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+// const request = require('request');
+// request('http://www.google.com', function (error, response, body) {
+//   console.error('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+// });
